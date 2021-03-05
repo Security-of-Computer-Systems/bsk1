09661c02af92694b6059df3e7145981dd21edafe
Kits0n <kitson1021.99@gmail.com>
2021-03-05 18:00:12 +0100

    GUI - cosmetic changes

--- src/GUI/AppWindow.py
+++ src/GUI/AppWindow.py
@@ -8,10 +8,11 @@ class AppWindow(QMainWindow):
         super(AppWindow, self).__init__(*args, **kwargs)
         self.window_width = 600
         self.window_height = 400
+        self.padding = 30
         self.mainUI()
         self.keysUI()
         self.senderUI()
-        self.recipientUI()
+        self.receiverUI()
         self.show()
         self.clickKeysButton()
 
@@ -19,20 +20,23 @@ class AppWindow(QMainWindow):
         self.setMinimumSize(QSize(self.window_width, self.window_height))
         self.setWindowTitle("BSK")
 
-        keys_button = QPushButton('Klucze', self)
-        keys_button.clicked.connect(self.clickKeysButton)
-        keys_button.resize(floor(self.window_width / 3), 32)
-        keys_button.move(0, 0)
+        self.keys_button = QPushButton('Keys', self)
+        self.keys_button.clicked.connect(self.clickKeysButton)
+        self.keys_button.resize(floor(self.window_width / 3), 32)
+        self.keys_button.move(0, 0)
+        self.keys_button.setStyleSheet("background-color : #21B0F7")
 
-        sender_button = QPushButton('Nadawca', self)
-        sender_button.clicked.connect(self.clickSenderButton)
-        sender_button.resize(floor(self.window_width / 3), 32)
-        sender_button.move(floor(self.window_width / 3), 0)
+        self.sender_button = QPushButton('Sender', self)
+        self.sender_button.clicked.connect(self.clickSenderButton)
+        self.sender_button.resize(floor(self.window_width / 3), 32)
+        self.sender_button.move(floor(self.window_width / 3), 0)
+        self.sender_button.setStyleSheet("background-color : #21B0F7")
 
-        recipient_button = QPushButton('Odbiorca', self)
-        recipient_button.clicked.connect(self.clickRecipientButton)
-        recipient_button.resize(floor(self.window_width / 3), 32)
-        recipient_button.move(floor(self.window_width / 3 * 2), 0)
+        self.receiver_button = QPushButton('Receiver', self)
+        self.receiver_button.clicked.connect(self.clickReceiverButton)
+        self.receiver_button.resize(floor(self.window_width / 3), 32)
+        self.receiver_button.move(floor(self.window_width / 3 * 2), 0)
+        self.receiver_button.setStyleSheet("background-color : #21B0F7")
 
         self.show()
 
@@ -41,50 +45,50 @@ class AppWindow(QMainWindow):
         # Ok Label
         self.keys_ok_label = QLabel(self)
         self.keys_ok_label.setText('Ok')
-        self.keys_ok_label.move(floor(self.window_width / 3), floor(self.window_height / 10 * 2))
+        self.keys_ok_label.move(floor(self.window_width / 3)+self.padding, floor(self.window_height / 10 * 2))
 
         # Generate keys button
         self.generate_keys_button = QPushButton('Generate Keys', self)
         self.generate_keys_button.clicked.connect(self.clickGenerateKeysButton)
         self.generate_keys_button.resize(floor(self.window_width / 3), 32)
-        self.generate_keys_button.move(0, floor(self.window_height / 10 * 2))
+        self.generate_keys_button.move(self.padding, floor(self.window_height / 10 * 2))
 
         # Ip Label
         self.keys_ip_label = QLabel(self)
         self.keys_ip_label.setText('Ip')
-        self.keys_ip_label.move(0, floor(self.window_height / 10 * 4))
+        self.keys_ip_label.move(self.padding, floor(self.window_height / 10 * 4)-32)
 
         # Ip Line Edit
         self.keys_ip = QLineEdit(self)
-        self.keys_ip.move(0, floor(self.window_height / 10 * 5))
+        self.keys_ip.move(self.padding, floor(self.window_height / 10 * 4))
         self.keys_ip.resize(floor(self.window_width / 3), 32)
 
         # Public key label
         self.public_key_label = QLabel(self)
         self.public_key_label.setText('Public Key')
-        self.public_key_label.move(floor(self.window_width / 3), floor(self.window_height / 10 * 4))
+        self.public_key_label.move(self.padding, floor(self.window_height / 10 * 6)-32)
 
         # Public Key Line Edit
         self.public_key = QLineEdit(self)
-        self.public_key.move(floor(self.window_width / 3), floor(self.window_height / 10 * 5))
+        self.public_key.move(self.padding, floor(self.window_height / 10 * 6))
         self.public_key.resize(floor(self.window_width / 3), 32)
 
         # Choose Public key file
         self.choose_public_key_button = QPushButton('Choose file', self)
         self.choose_public_key_button.clicked.connect(self.choosePublicKey)
         self.choose_public_key_button.resize(floor(self.window_width / 3), 32)
-        self.choose_public_key_button.move(floor(self.window_width / 3*2), floor(self.window_height / 10 * 5))
+        self.choose_public_key_button.move(floor(self.window_width / 3)+self.padding, floor(self.window_height / 10 * 6))
 
         # Send button
         self.send_key_button = QPushButton('Send', self)
         self.send_key_button.clicked.connect(self.sendKey)
         self.send_key_button.resize(floor(self.window_width / 3), 32)
-        self.send_key_button.move(floor(self.window_width / 3), floor(self.window_height / 10 * 7))
+        self.send_key_button.move(floor(self.window_width / 3), floor(self.window_height / 10 * 8))
 
         # Send Label
         self.send_key_label = QLabel(self)
         self.send_key_label.setText('Ok')
-        self.send_key_label.move(floor(self.window_width / 3*2), floor(self.window_height / 10 * 7))
+        self.send_key_label.move(floor(self.window_width / 3*2), floor(self.window_height / 10 * 8))
 
     def senderUI(self):
         self.sender_label = QLabel(self)
@@ -92,16 +96,16 @@ class AppWindow(QMainWindow):
         self.sender_label.move(80, 100)
         self.sender_label.resize(700, 32)
 
-    def recipientUI(self):
-        self.recipient_label = QLabel(self)
-        self.recipient_label.setText('Recipent')
-        self.recipient_label.move(80, 100)
-        self.recipient_label.resize(700, 32)
+    def receiverUI(self):
+        self.receiver_label = QLabel(self)
+        self.receiver_label.setText('Receiver')
+        self.receiver_label.move(80, 100)
+        self.receiver_label.resize(700, 32)
 
     def hideAll(self):
         self.keys_ok_label.hide()
         self.sender_label.hide()
-        self.recipient_label.hide()
+        self.receiver_label.hide()
         self.generate_keys_button.hide()
         self.keys_ip_label.hide()
         self.keys_ip.hide()
@@ -113,6 +117,9 @@ class AppWindow(QMainWindow):
 
     def clickKeysButton(self):
         self.hideAll()
+        self.keys_button.setStyleSheet("background-color : #2C93C7")
+        self.sender_button.setStyleSheet("background-color : #21B0F7")
+        self.receiver_button.setStyleSheet("background-color : #21B0F7")
         self.generate_keys_button.show()
         self.keys_ip_label.show()
         self.keys_ip.show()
@@ -121,13 +128,20 @@ class AppWindow(QMainWindow):
         self.choose_public_key_button.show()
         self.send_key_button.show()
 
+
     def clickSenderButton(self):
         self.hideAll()
+        self.sender_button.setStyleSheet("background-color : #2C93C7")
+        self.keys_button.setStyleSheet("background-color : #21B0F7")
+        self.receiver_button.setStyleSheet("background-color : #21B0F7")
         self.sender_label.show()
 
-    def clickRecipientButton(self):
+    def clickReceiverButton(self):
         self.hideAll()
-        self.recipient_label.show()
+        self.receiver_button.setStyleSheet("background-color : #2C93C7")
+        self.sender_button.setStyleSheet("background-color : #21B0F7")
+        self.keys_button.setStyleSheet("background-color : #21B0F7")
+        self.receiver_label.show()
 
     def clickGenerateKeysButton(self):
         ##########################
